bdd07056fd533e7a3b4287959b0c05fecb72c52d
Amy Qin <amyqin501@gmail.com>
2024-06-23 14:50:46 -0400

    Add post endpoint

--- next.config.js
+++ next.config.js
@@ -5,12 +5,20 @@ const nextConfig = {
     async headers() {
         return [
             {
-                // matching all API routes
                 source: "/api/actions",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Origin", value: "*" }, 
                     { key: "Access-Control-Allow-Methods", value: "GET" },
+                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-Type, Date" },
+                ]
+            },
+            {
+                source: "/api/mint",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "POST" },
                     { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-Type, Date" },
                 ]
             }
