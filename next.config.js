e5a66e31e20c821954b0db3a30b1f6a7243f7b4f
Amy Qin <amyqin501@gmail.com>
2024-06-24 22:05:58 -0400

    Please

--- next.config.js
+++ next.config.js
@@ -20,7 +20,6 @@ const nextConfig = {
                 headers: [
                     { key: "Access-Control-Allow-Origin", value: "*" }, 
                     { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,OPTIONS" },
-                    { key: "Accept-Encoding", value: "compress" },
                     { key: "Content-Type", value: "application/json" },
                     { key: "Content-Encoding", value: "compress" },
                     { key: "Accept-Encoding", value: "compress" },
@@ -31,7 +30,6 @@ const nextConfig = {
                 headers: [
                     { key: "Access-Control-Allow-Origin", value: "*" }, 
                     { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,OPTIONS" },
-                    { key: "Accept-Encoding", value: "compress" },
                     { key: "Content-Type", value: "application/json" },
                     { key: "Content-Encoding", value: "compress" },
                     { key: "Accept-Encoding", value: "compress" },
