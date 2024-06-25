693baa68848f43c2c2db4bbe4cc3b405123d1793
Amy Qin <amyqin501@gmail.com>
2024-06-25 13:07:17 -0400

    Add config

--- next.config.js
+++ next.config.js
@@ -38,6 +38,18 @@ const nextConfig = {
                     { key: "Referrer-Policy", value: "no-referrer" },
                     { key: "Access-Control-Allow-Headers", value: "Accept, Content-Type, Authorization, Content-Encoding, Accept-Encoding"}
                 ]
+            },
+            {
+                source: "/api/getReading",
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "*" }, 
+                    { key: "Access-Control-Allow-Methods", value: 'GET,POST,PUT,OPTIONS' },
+                    { key: "Content-Type", value: "application/json" },
+                    { key: "Content-Encoding", value: "compress" },
+                    { key: "Accept-Encoding", value: "compress" },
+                    { key: "Referrer-Policy", value: "no-referrer" },
+                    { key: "Access-Control-Allow-Headers", value: "Accept, Content-Type, Authorization, Content-Encoding, Accept-Encoding"}
+                ]
             }
         ]
     },
