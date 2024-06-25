105cc66055ad0786970bf88a80dbb93e004bdb04
Amy Qin <amyqin501@gmail.com>
2024-06-24 21:49:17 -0400

    Get help from docs

--- next.config.js
+++ next.config.js
@@ -8,29 +8,32 @@ const nextConfig = {
                 source: "/api/actions",
                 headers: [
                     { key: "Access-Control-Allow-Origin", value: "*" }, 
-                    { key: "Access-Control-Allow-Methods", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,OPTIONS" },
                     { key: "Content-Type", value: "application/json" },
                     { key: "Content-Encoding", value: "compress" },
                     { key: "Accept-Encoding", value: "compress" },
+                    { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, Content-Encoding, Accept-Encoding"}
                 ]
             },
             {
                 source: "/api/mint",
                 headers: [
                     { key: "Access-Control-Allow-Origin", value: "*" }, 
-                    { key: "Access-Control-Allow-Methods", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,OPTIONS" },
                     { key: "Accept-Encoding", value: "compress" },
                     { key: "Content-Type", value: "application/json" },
                     { key: "Content-Encoding", value: "compress" },
+                    { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, Content-Encoding, Accept-Encoding"}
                 ]
             },            {
                 source: "/api/donate",
                 headers: [
                     { key: "Access-Control-Allow-Origin", value: "*" }, 
-                    { key: "Access-Control-Allow-Methods", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,OPTIONS" },
                     { key: "Accept-Encoding", value: "compress" },
                     { key: "Content-Type", value: "application/json" },
                     { key: "Content-Encoding", value: "compress" },
+                    { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, Content-Encoding, Accept-Encoding"}
                 ]
             }
         ]
