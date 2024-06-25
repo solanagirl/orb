76a3261c6a0ee2e3babade874638b9e815b795ba
Amy Qin <amyqin501@gmail.com>
2024-06-24 20:58:50 -0400

    She adds headers to nextconfig

--- next.config.js
+++ next.config.js
@@ -2,6 +2,36 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/api/actions",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, 
+                    { key: "Access-Control-Allow-Methods", value: "*" },
+                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-Type, Date" },
+                ]
+            },
+            {
+                source: "/api/mint",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "*" },
+                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-Type, Date" },
+                ]
+            },            {
+                source: "/api/donate",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, 
+                    { key: "Access-Control-Allow-Methods", value: "*" },
+                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-Type, Date" },
+                ]
+            }
+        ]
+    },
     optimizeFonts: false,
 }
 
--- pages/api/actions.ts
+++ pages/api/actions.ts
@@ -62,7 +62,7 @@ export default function handler(
     res.status(200).json({ 
         "title": "Orb Reading",
         "icon": "https://shdw-drive.genesysgo.net/G1Tzt42SDqCV3x9vPY5X826foA8fEk8BR4bB5wARh75d/orb2.PNG",
-        "description": "Ask the Orb with your SOL to get a reading. May Fortune favor you. Donate to support.",
+        "description": "Ask the Orb for answers to your question. Donate SOL to keep the Orb spinning.",
         "label": "Ask",
         "links": {
             "actions": [
