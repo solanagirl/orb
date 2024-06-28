75329e44ad20de74f4297adfd7823f0dbd021d07
Amy Qin <amyqin501@gmail.com>
2024-06-28 14:21:11 -0400

    Try webhook fix

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,6 +1,8 @@
-export async function POST(request: Request) {
+export async function handler(request: Request) {
+    if (request.method == 'POST') {
     const { body } = request;
     console.log(body);
     return Response.json("cNFT burned");
+    }
 }
   
